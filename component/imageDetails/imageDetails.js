9700f97a084efab6acea8c4af684fd864b573412
gaoqiquan <g2994936517@163.com>
2019-05-21 18:00:46 +0800

    添加页面5.21相册功能

---
+++ component/imageDetails/imageDetails.js
@@ -0,0 +1,48 @@
+import React, {PureComponent, Component} from 'react'
+import {View, Modal,Navigator,Text,Image} from 'react-native';
+import ImageViewer from 'react-native-image-zoom-viewer';
+
+/*const images = [{
+    url: "file:///storage/emulated/0/Android/data/com.escan/files/Pictures/image-1466da83-7085-486a-a0e6-bd7cba2b963e.jpg",
+},
+    {url: "file:///storage/emulated/0/Android/data/com.escan/files/Pictures/image-1466da83-7085-486a-a0e6-bd7cba2b963e.jpg"}
+
+];*/
+
+export default class imageDetails extends PureComponent {
+
+    state = {
+        index: 0,
+        modalVisible: true
+    };
+
+    render() {
+        const {navigation} = this.props;
+        //通过navigation.getParam来获取传递过来的参数
+        const itemId = navigation.getParam('imgUrl', 'no-values');
+        let ImageObjArray = [];
+        for (let i = 0; i < itemId.img.length; i++) {
+            let Obj = {};
+            Obj.url = itemId.img[i];
+            this.state.index=i
+            ImageObjArray.push(Obj)
+        }
+        return (
+            <View
+                style={{
+                    padding: 10
+                }}
+            >
+               <Modal
+                    visible={this.state.modalVisible}
+                    transparent={true}
+                    onRequestClose={() => this.setState({modalVisible: false})}>
+                    <ImageViewer
+                        enableImageZoom={true}
+                        onClick={()=>this.props.navigation.goBack()}
+                        imageUrls={ImageObjArray} index={this.state.index}/>
+                </Modal>
+            </View>
+        )
+    }
+}
--- component/paymentContractUpdate.js
+++ component/paymentContractUpdate.js
@@ -12,19 +12,47 @@ import {
     Modal,
     ImageBackground,
     Alert,
+    DatePickerAndroid,
+    TouchableHighlight,
     Input,
 } from 'react-native';
 import ImagePicker from 'react-native-image-picker';
 import ImageViewer from 'react-native-image-zoom-viewer';
+import {createAppContainer, createStackNavigator} from "react-navigation";
+import PaymentInformation from "./paymentInformation";
+import BillingInformationDetails from "./billingInformationDetails";
+import LoadingImage from "./loadingImage";
+import ImageDetails from '../component/imageDetails/imageDetails'
 
 var {width,height} = Dimensions.get("window");//第一种写法
-export default class paymentContractUpdate extends React.Component{
+var name =["1","2","3"]
+class CustomButton extends React.Component {
+    render() {
+        return (
+            <TouchableHighlight
+                style={styles.button}
+                underlayColor="#a5a5a5"
+                onPress={this.props.onPress}>
+                <Text style={[styles.buttonText]}>{this.props.text}</Text>
+            </TouchableHighlight>
+        );
+    }
+}
+ class paymentContractUp extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
             inputValue: "12.32",
+            MValue:'',
+            avatarSource: [],
+            array:[],
+            videoSource: null,
+            i:0,
+            presetDate: new Date(),
+            presetText: this.formateDate(new Date()),
         }
     }
+
     render(){
         return(
             <View  style={{flex:1,backgroundColor:'#eeeeee'}}>
@@ -82,16 +110,13 @@ export default class paymentContractUpdate extends React.Component{
                     <View style={{backgroundColor:'white',marginTop:10}}>
                         <View>
                             <Text style={{marginLeft:'3%',fontSize:15,marginTop:10,marginBottom:10}}>签订时间：</Text>
+                            <CustomButton  text={this.state.presetText} onPress={this.showPicker.bind(this, 'preset', {date: this.state.presetDate})}>
                             <View style={{marginLeft:'3%',marginRight:'3%',fontSize:15}}>
-                                <TextInput
-                                           placeholder="请输入签订时间"
-                                           editable={true}//是否可编辑
-                                           underlineColorAndroid='#E5E5E5'
-                                           style={{fontSize:14,height: 40}}
-                                           defaultValue='2019-12-21'
-                                />
                             </View>
+                            </CustomButton>
                         </View>
+                    </View>
+                    <View style={{backgroundColor:'white',marginTop:10}}>
                         <View>
                             <Text style={{marginLeft:'3%',fontSize:15,marginTop:10,marginBottom:10}}>合同金额：</Text>
                             <View style={{marginLeft:'3%',marginRight:'3%',fontSize:15}}>
@@ -101,10 +126,46 @@ export default class paymentContractUpdate extends React.Component{
                                     underlineColorAndroid='#E5E5E5'
                                     style={{fontSize:14,height: 40}}
                                     onChangeText={(text) => {
-                                        const newText = text.replace(/[^\d]+/, '');
+                                       let newText = (text != '' && text.substr(0,1) == '.') ? '' : text;
+                                        /*newText = newText.replace(/[^\d.]/g,""); //清除"数字"和"."以外的字符
                                         //可以打印看看是否过滤掉了非数字
+                                        console.log(newText)*/
                                         console.log(newText)
-                                        this.setState({inputValue: newText})
+                                        this.setState({MValue: newText})
+                                    }}
+                                    onBlur={()=>{//||!this.state.MValue.matches(/[^\d.]/g)
+                                        var str = this.state.MValue;
+                                        var len1 = str.substr(0, 1);
+                                        var len2 = str.substr(1, 1);
+                                        //如果第一位是0，第二位不是点，就用数字把点替换掉
+                                        if (str.length > 1 && len1 == 0 && len2 != ".") {
+                                            this.setState({inputValue: this.state.inputValue})
+                                            alert('输入格式错误');
+                                            return false
+                                        }
+                                        //第一位不能是.
+                                        if (len1 == ".") {
+                                            this.setState({inputValue: this.state.inputValue})
+                                            alert('输入格式错误');
+                                            return false
+                                        }
+                                        //限制只能输入一个小数点
+                                        if (str.indexOf(".") != -1) {
+                                            var str_ = str.substr(str.indexOf(".") + 1);
+                                            if (str_.indexOf(".") != -1) {
+                                                this.setState({inputValue: this.state.inputValue})
+                                                /*str = str.substr(0, str.indexOf(".") + str_.indexOf(".") + 1);*/
+                                                alert('输入格式错误');
+                                                return false
+                                            }
+
+                                        }
+                                        if(str.replace(/[^a-zA-Z]/g, '').length>0){
+                                            this.setState({inputValue: this.state.inputValue})
+                                            alert('输入格式错误');
+                                            return false
+                                        }
+                                        this.state.inputValue=this.state.MValue
                                     }}
                                     defaultValue={this.state.inputValue}
                                 />
@@ -155,7 +216,16 @@ export default class paymentContractUpdate extends React.Component{
                                 </TouchableOpacity>
                                 <View style={[styles.avatarContainer, {marginBottom: 30}]}>
                                     <ScrollView horizontal>
-                                        {
+                                       {/* <TouchableOpacity onLongPress={()=>this.longPress()}>
+                                            <Text>{name[0]}</Text>
+                                        </TouchableOpacity>
+                                        <TouchableOpacity onLongPress={()=>this.longPress()}>
+                                            <Text>{name[1]}</Text>
+                                        </TouchableOpacity>
+                                        <TouchableOpacity onLongPress={()=>this.longPress()}>
+                                            <Text>{name[2]}</Text>
+                                        </TouchableOpacity>*/}
+                                            {
                                             this.selectPhoto()
                                         }
                                     </ScrollView>
@@ -168,12 +238,39 @@ export default class paymentContractUpdate extends React.Component{
             </View>
         )
     }
-    state = {
+
+     async showPicker(stateKey, options) {
+         try {
+             var newState = {};
+             var date = new Date();
+             const {action, year, month, day} = await DatePickerAndroid.open(options);
+             if (action === DatePickerAndroid.dismissedAction) {
+                /* newState[stateKey + 'Text'] = date;*/
+             } else {
+                 var date = new Date(year, month, day);
+                 newState[stateKey + 'Text'] = date.toLocaleDateString();
+                 newState[stateKey + 'Date'] = date;
+             }
+             this.setState(newState);
+         } catch ({code, message}) {
+             console.warn(`Error in example '${stateKey}': `, message);
+         }
+     }
+    /*state = {
         avatarSource: [],
+        array:[],
         videoSource: null,
         i:0,
 
-    };
+    };*/
+     formateDate(date) {
+         var year = date.getFullYear() ;
+         var month = date.getMonth() +1 ;
+         var day = date.getDate() ;
+         var formatedStr = year + '/' + month +'/' + day ;
+         // console.log('formatedStr: ' + formatedStr ) ;
+         return formatedStr ;
+     }
     //选择图片
     selectPhotoTapped() {
         const options = {
@@ -210,41 +307,94 @@ export default class paymentContractUpdate extends React.Component{
             }
             else {
                 let source = { uri: response.uri };
+                let arr = response.uri
                 this.setState({
+                    array:[arr],
                     avatarSource: [source]
+
                 });
+                console.log(this.state.avatarSource.length)
             }
         });
 
     }
     selectPhoto(){
         let images = this.state.avatarSource;
+        let img = this.state.array;
         if(flug){
             flug=false
             return pages
         }else{
-            pages.push( <TouchableOpacity onPress={()=>this.magnifyImage(images)}><Image style={styles.avatar} source={images} /></TouchableOpacity>);
+            pages.push( <TouchableOpacity onLongPress={()=>this.longPress()} key={images.index}  onPress={()=>this.props.navigation.navigate('imageDetails',{imgUrl: {img}})}>
+                <Image style={styles.avatar} source={images} />
+            </TouchableOpacity>);
         }
         return pages
     }
+
+    //删除方法
+     longPress(){
+       /* let count = 0;
+        for(let j=0;j<this.state.array.length;j++){
+            console.log(img[0]+"---"+this.state.array[j]+"---"+this.state.array.length)
+            if(img[0] == this.state.array[j]){
+                count=j
+            }
+        }*/
+       console.log(this.state.avatarSource.length+"=="+JSON.stringify(this.state.avatarSource)+"=="+this.state.avatarSource[0])
+         console.log(JSON.stringify(this.state.avatarSource[0])+"=="+this.state.avatarSource.index)
+         Alert.alert('请选择','是否删除该图片'+this.state.avatarSource.length,
+             [
+                 {
+                     text: "确认",
+                     onPress:this.onselect()
+                 },
+                 {text:"取消"},
+             ]
+         );
+     }
+     onselect(){
+         alert('123')
+         let a = this.state.avatarSource
+            delete a[0]
+         this.setState({
+            avatarSource:[]
+         })
+         console.log(JSON.stringify(this.state.avatarSource[0]))
+         /*let arr = this.state.avatarSource
+                         arr.splice(0,1)
+                        this.setState({
+                            avatarSource:this.state.avatarSource
+                        })*/
+     }
     state = {
         index: 0,
         modalVisible: true
     };
-    magnifyImage(images){
-        xianshi=(<View
-            style={{
-                padding: 10
-            }}
-        >
-            <Modal
-                visible={this.state.modalVisible}
-                transparent={true}
-                onRequestClose={() => this.setState({modalVisible: false})}>
-                <ImageViewer imageUrls={images} index={this.state.index}/>
-            </Modal>
-        </View>);
-        return  xianshi
+}
+
+const App = createStackNavigator(
+    {
+        paymentContractUp: {
+            screen:paymentContractUp,
+            navigationOptions: {  // 屏幕导航的默认选项, 也可以在组件内用 static navigationOptions 设置(会覆盖此处的设置)
+                header: null,  //隐藏导航栏
+            }
+        },
+        imageDetails:{
+            screen:ImageDetails,
+            navigationOptions: {  // 屏幕导航的默认选项, 也可以在组件内用 static navigationOptions 设置(会覆盖此处的设置)
+                header: null,  //隐藏导航栏
+            }
+        }
+    },{
+        initialRouteName: 'paymentContractUp',
+    });
+
+const AppContainer = createAppContainer(App);
+export default class paymentContractUpdate extends React.Component {
+    render() {
+        return <AppContainer />;
     }
 }
 var xianshi=null;
@@ -268,6 +418,12 @@ const styles = StyleSheet.create({
         width:200,
         height: 120
     },
-
+    button: {
+        margin:5,
+        backgroundColor: 'white',
+        padding: 15,
+        borderBottomWidth: StyleSheet.hairlineWidth,
+        borderBottomColor: '#cdcdcd',
+    }
 
 });
